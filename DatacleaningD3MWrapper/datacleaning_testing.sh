e1f7b0121a8c6e03868d219bb7ebcd2d6d60f38b
Sanjeev Namjoshi <snamjoshi87@utexas.edu>
2020-01-14 16:30:49 -0600

    Added new pipeline to handle semantic types

--- DatacleaningD3MWrapper/datacleaning_testing.sh
+++ DatacleaningD3MWrapper/datacleaning_testing.sh
@@ -1,17 +1,13 @@
 #!/bin/bash -e
 
-Datasets=('30_personae','196_autoMpg','185_baseball','26_radon_seed','38_sick','4550_miceProtein','LL0_207_autoPrice','57_hypothyroid','1491_one_hundred_plants_margin','LL0_acled_reduced','LL0_1100_popularkids')
+Datasets=('30_personae' '196_autoMpg' '185_baseball' '26_radon_seed' '38_sick' '4550_miceProtein' 'LL0_207_autoPrice' '57_hypothyroid' '1491_one_hundred_plants_margin' 'LL0_acled_reduced' 'LL0_1100_popularkids')
 
-cd "/src/dataclean/DatacleaningD3MWrapper/"
-
-# create text file to record scores and timing information
-touch pipeline_tests/scores.txt
-echo "DATASET, SCORE, EXECUTION TIME" >> pipeline_tests/scores.txt
+cd "/dataclean/"
 
 for i in "${Datasets[@]}"; do
 
     # generate and save pipeline
-    python3 "/src/dataclean/DatacleaningD3MWrapper/pipeline.py" $i
+    python3 "/dataclean/pipeline.py" $i
 
     # test and score pipeline    
     start=`date +%s`
@@ -19,13 +15,9 @@ for i in "${Datasets[@]}"; do
     end=`date +%s`
     runtime=$((end-start))
     
-    IFS=, read col1 score col3 col4 < <(tail -n1 scores.csv)
-        echo "$i, $score, $runtime" >> pipeline_tests/scores.txt
-
     # cleanup temporary file
     mv *.json pipeline_tests/${i}_pipeline.json
     mv ${i}_pipeline_run.yaml pipeline_tests/${i}_pipeline_run.yaml
-    cp "/src/dataclean/DatacleaningD3MWrapper/pipeline.py" "/src/dataclean/DatacleaningD3MWrapper/pipeline_tests/${i}_pipeline.py"
     rm *.meta
     rm scores.csv  
 done
